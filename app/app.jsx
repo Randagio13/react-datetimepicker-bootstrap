f0a38fc0e1e09a3497e6a3f9185a63a4ed6d05bb
Alessandro Casazza <imasterale@gmail.com>
2015-11-25 08:21:56 +0100

    Fix minDate maxDate options

--- app/app.jsx
+++ app/app.jsx
@@ -4,5 +4,5 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import DateTimePicker from './component/DateTimePicker';
 console.log(DateTimePicker);
-ReactDom.render(<DateTimePicker id="datetimepicker" icon="left" allowInputToggle />, document.getElementById('app'));
+ReactDom.render(<DateTimePicker id="datetimepicker" icon="left" allowInputToggle minDate={new Date()} maxDate={new Date('2015/11/30')} />, document.getElementById('app'));
 // export default DateTimePicker;
