d0a2ad291a11dc96f3d3efc19f052db8f1756681
Alessandro Casazza <imasterale@gmail.com>
2015-11-25 02:52:50 +0100

    Prepare v0.2.0

--- app/app.jsx
+++ app/app.jsx
@@ -1,8 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css';
-import React from 'react';
-import ReactDom from 'react-dom';
 import DateTimePicker from './component/DateTimePicker';
-console.log(DateTimePicker);
-ReactDom.render(<DateTimePicker id="datetimepicker" icon="left" allowInputToggle />, document.getElementById('app'));
-// export default DateTimePicker;
+export default DateTimePicker;
