b577fabc3af2c6dd6e36135b7fa7f245d5347844
Alessandro Casazza <imasterale@gmail.com>
2015-12-01 03:01:49 +0100

    env test babel

--- app/app.jsx
+++ app/app.jsx
@@ -1,8 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css';
 import DateTimePicker from './component/DateTimePicker';
-console.log(process.env.BABEL_ENV);
-if (process.env.BABEL_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production') {
     ReactDOM.render(<DateTimePicker id="datetimepicker" />, document.getElementById('app'));
 }
 export default DateTimePicker;
