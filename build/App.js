27dcc30ef00ed5db2f3497f4ebb3e416994f6ea1
Alessandro Casazza <imasterale@gmail.com>
2015-12-01 02:45:23 +0100

    test env

--- build/App.js
+++ build/App.js
@@ -14,8 +14,8 @@ var _DateTimePicker2 = _interopRequireDefault(_DateTimePicker);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-console.log(process.env.BABEL_ENV);
-if (process.env.BABEL_ENV !== 'production') {
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV !== 'production') {
     ReactDOM.render(React.createElement(_DateTimePicker2.default, { id: 'datetimepicker' }), document.getElementById('app'));
 }
 exports.default = _DateTimePicker2.default;
